4756cc1cd3c0497852128535a4c42adf4a5a1bc5
ni1i <77991672+ni1i@users.noreply.github.com>
2021-02-26 11:40:04 +0300

    Update nfsc_script.sh

--- 1.7/nfsc_script.sh
+++ 1.7/nfsc_script.sh
@@ -7,6 +7,6 @@ systemctl enable rpcbind
 
 mkdir /mnt/nfs_share
 
-mount -t nfs 192.168.50.10:/var/nfs_share/ /mnt/nfs_share/
+echo "192.168.50.10:/var/nfs_share/ /mnt/nfs_share/ nfs nfsvers=3 0 0" >> /etc/fstab
 
-echo "192.168.50.10:/var/nfs_share/ /mnt/nfs_share/ nfs defaults 0 0" >> /etc/fstab
+mount -a
