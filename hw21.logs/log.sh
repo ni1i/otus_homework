51da6f4c7dfebe2ee3d9380dee4c590448266167
ni1i <kurtov@protonmail.com>
2021-04-22 16:59:06 +0300

    .

---
+++ hw21.logs/log.sh
@@ -0,0 +1,15 @@
+sudo yum update -y
+sudo yum install epel-release -y
+sudo yum install rsyslog -y
+sudo systemctl enable rsyslog
+sudo systemctl start rsyslog
+sudo sed -i '/.*#tcp_listen_port.*/a tcp_listen_port = 60' /etc/audit/auditd.conf
+sudo sed -i 's/#$ModLoad imudp/$ModLoad imudp/' /etc/rsyslog.conf
+sudo sed -i 's/#$UDPServerRun 514/$UDPServerRun 514/' /etc/rsyslog.conf
+sudo sed -i 's/#$ModLoad imtcp/$ModLoad imtcp/' /etc/rsyslog.conf
+sudo sed -i 's/#$InputTCPServerRun 514/$InputTCPServerRun 514/' /etc/rsyslog.conf
+sudo echo "\$template RemoteLogs,\"/var/log/%HOSTNAME%/%PROGRAMNAME%.log\"" >> /etc/rsyslog.conf
+sudo echo "*.* ?RemoteLogs" >> /etc/rsyslog.conf
+sudo echo "& ~" >> /etc/rsyslog.conf
+sudo service auditd restart
+sudo systemctl restart rsyslog
